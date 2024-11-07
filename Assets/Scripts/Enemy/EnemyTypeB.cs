19a96dfce3d9ae36be681bc15a955e65ef702a52
MiguelAn05 <miguelingil89@gmail.com>
2024-11-06 19:37:08 -0500

    enemigo volador

--- Assets/Scripts/Enemy/EnemyTypeB.cs
+++ Assets/Scripts/Enemy/EnemyTypeB.cs
@@ -1,12 +1,20 @@
 using UnityEngine;
+using UnityEngine.Serialization;
 
 public class EnemyTypeB : EnemyBase
 {
+    
+    [SerializeField] private float minAltitude = 3.0f;
+    [SerializeField] private float maxAltitude = 15.0f;
+    
     protected override void MoveTowardsPlayer(Transform player)
     {
+      float targetY = Mathf.Clamp(player.position.y + minAltitude, minAltitude, maxAltitude);
         Vector3 direction = (player.position - transform.position).normalized;
-        Vector3 flyPosition = new Vector3(direction.x, direction.y + 0.5f, direction.z);
-        transform.position = Vector3.MoveTowards(transform.position, player.position + flyPosition, moveSpeed * Time.deltaTime);
+       
+        Vector3 targetPosition = new Vector3(player.position.x, targetY, player.position.z);
+
+        transform.position = Vector3.MoveTowards(transform.position, targetPosition, moveSpeed * Time.deltaTime);
         
     }
 
